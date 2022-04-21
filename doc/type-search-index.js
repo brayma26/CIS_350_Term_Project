c37653faa591e1612101b314f7725a530e4d7222
brayma26 <brayma@mail.gvsu.edu>
2022-04-21 09:48:16 -0400

    Added correct Document folder

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"main","l":"Background"},{"p":"main","l":"Car"},{"p":"main","l":"Game"},{"p":"main","l":"Obstacle"},{"p":"main","l":"PowerUp"},{"p":"main","l":"Sound"}];updateSearchResults();
